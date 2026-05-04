156baa118671155c85b57499901ddae183634ab3
Jack Smith <jackwsmth@proton.me>
2026-05-04 10:01:14 +0100

    Create highlights query

---
+++ queries/highlights.scm
@@ -0,0 +1,19 @@
+(from_instruction) @keyword
+(parameter_instruction) @keyword
+(requires_instruction) @keyword
+(message_instruction) @keyword
+(template_instruction) @keyword
+(system_instruction) @keyword
+(adapter_instruction) @keyword
+(license_instruction) @keyword
+
+(argument) @variable
+
+(parameter_value) @string
+
+(number) @number
+(quoted_string) @string
+
+(multiline_string) @string
+
+(comment) @comment
