b35b192360b88bcf82e44206f9e3ee73438032c1
Jack Smith <jackwsmth@proton.me>
2026-05-06 15:43:40 +0100

    Add trailing comma

--- grammar.js
+++ grammar.js
@@ -22,7 +22,7 @@ export default grammar({
         $.template_instruction,
         $.system_instruction,
         $.adapter_instruction,
-        $.license_instruction
+        $.license_instruction,
       ),
 
     from_instruction: ($) => seq("FROM", $.model_source),
